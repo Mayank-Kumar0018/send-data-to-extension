c7beabd059604883f28c2e8c8d40b13eea694b34
Mayank-kumar001 <dev.01mayank@gmail.com>
2025-04-19 18:32:34 +0530

    minor changes

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const urlParams = new URLSearchParams(window.location.search);
 const token = urlParams.get("token");
 
-chrome.runtime.sendMessage({ type: "OAUTH_SUCCESS", token });
+// chrome.runtime.sendMessage({ type: "OAUTH_SUCCESS", token });
+window.postMessage({ type: "OAUTH_SUCCESS", token }, "*");
