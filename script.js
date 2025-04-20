5ec0d65bd9a270b345dbbb631d6dd879f1fcd89c
Mayank-kumar001 <dev.01mayank@gmail.com>
2025-04-20 11:26:42 +0530

    more changes to script

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ const token = urlParams.get("token");
 
 // chrome.runtime.sendMessage({ type: "OAUTH_SUCCESS", token });
 window.postMessage({ type: "OAUTH_SUCCESS", token }, "*");
+console.log("outh token : ",  token);
 console.log("hello world sending tokens :)")
